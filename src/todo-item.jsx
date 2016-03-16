6a571ce6fa2a41b058e6bfcb6ddae5f334b7e7fa
aallison <aallison@yahoo-inc.com>
2016-03-16 12:49:14 -0700

    fix whitespace

--- src/todo-item.jsx
+++ src/todo-item.jsx
@@ -2,18 +2,18 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  displayName: 'TodoItem',
+    displayName: 'TodoItem',
 
-  getInitialState: function() {
-    return { done: this.props.done }
-  },
+    getInitialState: function() {
+        return { done: this.props.done }
+    },
 
-  render: function() {
-    return (
-      <label>
-        <input type="checkbox" defaultChecked={ this.state.done } />
-        { this.props.name }
-      </label>
-    );
-  }
-});
+    render: function() {
+        return (
+            <label>
+                <input type="checkbox" defaultChecked={ this.state.done } />
+                { this.props.name }
+            </label>
+        )
+    }
+})
--- test/todo-item-spec.jsx
+++ test/todo-item-spec.jsx
@@ -11,40 +11,40 @@ var TodoItem = require('../src/todo-item')
 
 // see: https://github.com/jesstelford/react-testing-mocha-jsdom
 describe('todo-item', function() {
-	beforeEach(function() {
+    beforeEach(function() {
 
-		// reset jsdom document
+        // reset jsdom document
 
-		// A super simple DOM ready for React to render into
-		// Store this DOM and the window in global scope ready for React to access
-		global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
-		global.window = document.parentWindow
-	})
+        // A super simple DOM ready for React to render into
+        // Store this DOM and the window in global scope ready for React to access
+        global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
+        global.window = document.parentWindow
+    })
 
-	for (var isDone of [ true, false ]) {
-		describe('done==' + isDone, function() {
-			beforeEach(function() {
-				// render test elem
-				var renderedComponent = ReactTestUtils.renderIntoDocument(
-					<TodoItem done={ isDone } name="Write Tutorial"/>
-				)
+    for (var isDone of [ true, false ]) {
+        describe('done==' + isDone, function() {
+            beforeEach(function() {
+                // render test elem
+                var renderedComponent = ReactTestUtils.renderIntoDocument(
+                    <TodoItem done={ isDone } name="Write Tutorial"/>
+                )
 
-				// Searching for <input> tag within rendered React component
-				// Throws an exception if not found
-				this.inputComponent = ReactTestUtils.findRenderedDOMComponentWithTag(
-					renderedComponent,
-					'input'
-				)
-			})
+                // Searching for <input> tag within rendered React component
+                // Throws an exception if not found
+                this.inputComponent = ReactTestUtils.findRenderedDOMComponentWithTag(
+                    renderedComponent,
+                    'input'
+                )
+            })
 
-			it('input should be a checkbox', function() {
-				assert.equal(this.inputComponent.type, 'checkbox')
-			})
+            it('input should be a checkbox', function() {
+                assert.equal(this.inputComponent.type, 'checkbox')
+            })
 
-			it('input is checked', function() {
-				assert.equal(this.inputComponent.checked, isDone)
-			})
-		})
-	}
+            it('input is checked', function() {
+                assert.equal(this.inputComponent.checked, isDone)
+            })
+        })
+    }
 
 })
